54ed7f9329058c83074e52859488b38c463fc811
stephanyofandhikaprasetyo <yofantetta@gmail.com>
2023-03-10 10:18:07 +0700

    commit V1.0)

---
+++ fileremote1.py
@@ -0,0 +1,6 @@
+user_data = {
+"arsene lupin": {"nik": 1234, "jenis_kelamin": "male", "tanggal_lahir": "1902-03-23"},
+"sherlock holmes": {"nik": 4321, "jenis_kelamin": "male", "tanggal_lahir": "1876-08-16"},
+"irene adler": {"nik": 6789, "jenis_kelamin": "female", "tanggal_lahir": "1884-10-07"}
+}
+
