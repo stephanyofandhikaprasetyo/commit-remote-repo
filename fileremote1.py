2b8752bdfd778e9b1384ddd9af3b0d821e48b973
stephanyofandhikaprasetyo <yofantetta@gmail.com>
2023-03-10 10:22:28 +0700

    (commit V1.0)

--- fileremote1.py
+++ fileremote1.py
@@ -4,9 +4,3 @@ user_data = {
 "irene adler": {"nik": 6789, "jenis_kelamin": "female", "tanggal_lahir": "1884-10-07"}
 }
 
-nama = input("Masukkan nama: ")
-nik = int(input("Masukkan NIK: "))
-jenis_kelamin = input("Masukkan jenis kelamin: ")
-tanggal_lahir = input("Masukkan tanggal lahir (format YYYY-MM-DD): ")
-
-user_data[nama] = {"nik": nik, "jenis_kelamin": jenis_kelamin, "tanggal_lahir": tanggal_lahir}
